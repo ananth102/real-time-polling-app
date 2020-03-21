f97fe5e4a4e697a2a8b29918b90971801c40aee0
ananth102 <ananthbashyam@Ananths-MacBook-Pro-2.local>
2020-03-20 18:19:23 -0700

    cors

--- server.js
+++ server.js
@@ -14,19 +14,19 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
+  // if (req.method === "OPTIONS") {
+  //   res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+  //   return res.status(200).json({});
+  // }
   next();
 });
+app.use(cors());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
